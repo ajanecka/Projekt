ea707f06932ec75c5b2004fb267882ec3b4fed7d
Agnieszka Janecka <agawierzba1@wp.pl>
2022-10-08 15:59:25 +0200

    Initial commit

---
+++ Kalulator.cpp
@@ -0,0 +1,15 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+        double x = 0, y = 0;
+        cout << "Wprowadz pierwsza liczbe rzeczywista: "<<endl;
+        cin >> x;
+        cout << "Wprowadz druga liczbe rzeczywista: "<<endl;
+        cin >> y;
+
+        cout << x << " + " << y << " = " << x + y << endl;
+        cout << x << " - " << y << " = " << x - y << endl;
+return 0;
