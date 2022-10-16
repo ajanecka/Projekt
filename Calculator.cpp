4f56be5c916da94a2b8410065076cbe74673bf20
Agnieszka Janecka <agawierzba1@wp.pl>
2022-10-16 16:32:32 +0200

    Deklaracja a,b

--- Calculator.cpp
+++ Calculator.cpp
@@ -3,7 +3,8 @@
 using namespace std;
 
 int main() {
-        
+int a,b;
+
 cout << "a + b = " << sum(a,b) << endl;
 cout << "a - b = " << diff(a,b) << endl;
 cout << "a * b = " << prod(a,b) << endl;
